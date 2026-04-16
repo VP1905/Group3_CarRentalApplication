8bceaaa9cff34a697a8cb3e38a4949b2e590c542
VP1905 <Vpatel0589@conestogac.on.ca>
2026-04-16 15:32:46 -0400

    All the required folder is being added in the Domain layer and the Infrastructure layer.

--- G3VehicleInventory.Application/obj/Debug/net8.0/G3VehicleInventory.Application.AssemblyInfo.cs
+++ G3VehicleInventory.Application/obj/Debug/net8.0/G3VehicleInventory.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G3VehicleInventory.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a4fda52c17df64cfe223b7b7733a112084a363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b895e9fef916d0f9b12846e425fccbd615ccc093")]
 [assembly: System.Reflection.AssemblyProductAttribute("G3VehicleInventory.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G3VehicleInventory.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
