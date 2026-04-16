65a4fda52c17df64cfe223b7b7733a112084a363
VP1905 <Vpatel0589@conestogac.on.ca>
2026-04-16 14:15:51 -0400

    This is the initial code file.

---
+++ G3_CarRentalApplication.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G3_CarRentalApplication.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
