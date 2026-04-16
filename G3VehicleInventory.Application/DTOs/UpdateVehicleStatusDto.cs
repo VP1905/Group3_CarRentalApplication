9e41f1beab565b710d1c2551839643915473f597
VP1905 <Vpatel0589@conestogac.on.ca>
2026-04-16 15:39:39 -0400

    All the DTO files is being added.

---
+++ G3VehicleInventory.Application/DTOs/UpdateVehicleStatusDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3VehicleInventory.Application.DTOs
+{
+    // Data Transfer Object used to update the status of an existing vehicle.
+    public class UpdateVehicleStatusDto
+    {
+        [Required]
+        public string Status { get; set; } = string.Empty;
+    }
+}
---
+++ G3VehicleInventory.Application/DTOs/VehicleDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G3VehicleInventory.Application.DTOs
+{
+    //DTO used to send vehicle information from the application layer to the API response
+    public class VehicleDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string VehicleCode { get; set; } = string.Empty;
+
+        [Required]
+        public int LocationId { get; set; }
+
+        [Required]
+        public string VehicleType { get; set; } = string.Empty;
+
+        [Required]
+        public string Status { get; set; } = string.Empty;
+    }
+}
