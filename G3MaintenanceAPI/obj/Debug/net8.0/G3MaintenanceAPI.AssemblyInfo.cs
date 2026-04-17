e5c62a9ad806382daa5823407867322f6e4ed817
Nirali999 <vasantisathvara0@gmail.com>
2026-04-16 23:36:01 -0400

    Add connection string using SSMS database GR3MaintenanceDb

--- G3MaintenanceAPI/obj/Debug/net8.0/G3MaintenanceAPI.AssemblyInfo.cs
+++ G3MaintenanceAPI/obj/Debug/net8.0/G3MaintenanceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0e5c4e14e110e6a2610826231ae677438d0188")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a5f66e416c5a462c47a1743576bd5b2875e54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
