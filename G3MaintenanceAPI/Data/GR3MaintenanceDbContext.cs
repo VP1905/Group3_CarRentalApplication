40a5f66e416c5a462c47a1743576bd5b2875e54b
Nirali999 <vasantisathvara0@gmail.com>
2026-04-16 23:25:09 -0400

    Add GR3MaintenanceDbContext for maintenance API

---
+++ G3MaintenanceAPI/Data/GR3MaintenanceDbContext.cs
@@ -0,0 +1,16 @@
+﻿using G3MaintenanceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace G3MaintenanceAPI.Data
+{
+    public class GR3MaintenanceDbContext : DbContext
+    {
+        public GR3MaintenanceDbContext(DbContextOptions<GR3MaintenanceDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GR3RepairHistory> RepairHistories { get; set; }
+    }
+}
