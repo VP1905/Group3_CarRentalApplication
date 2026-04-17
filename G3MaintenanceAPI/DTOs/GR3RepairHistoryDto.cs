d1aa54c8e500f48b8935b35a9918c001af6597b8
Nirali999 <vasantisathvara0@gmail.com>
2026-04-16 23:53:43 -0400

    Add GR3RepairHistoryDto for maintenance API

---
+++ G3MaintenanceAPI/DTOs/GR3RepairHistoryDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace G3MaintenanceAPI.DTOs
+{
+    public class GR3RepairHistoryDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int VehicleId { get; set; }
+
+        [Required]
+        public DateTime RepairDate { get; set; }
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Range(0.01, 100000)]
+        public decimal Cost { get; set; }
+
+        [Required]
+        public string PerformedBy { get; set; } = string.Empty;
+    }
+}
--- G3MaintenanceAPI/obj/Debug/net8.0/G3MaintenanceAPI.AssemblyInfo.cs
+++ G3MaintenanceAPI/obj/Debug/net8.0/G3MaintenanceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c719521935d27be112f7fe8885d30d885cd786c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d364c184b093a7d21c90cf35e5c3575649c9f0b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("G3MaintenanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
