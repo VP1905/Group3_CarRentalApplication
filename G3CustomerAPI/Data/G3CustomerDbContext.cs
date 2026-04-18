e33470fdb0d8662cdee1f20e56ac2be6e8734406
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-04-17 23:03:13 -0400

    Add G3CustomerDbContext with Customers DbSet

---
+++ G3CustomerAPI/Data/G3CustomerDbContext.cs
@@ -0,0 +1,15 @@
+﻿using G3CustomerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace G3CustomerAPI.Data
+{
+    public class G3CustomerDbContext : DbContext
+    {
+        public G3CustomerDbContext(DbContextOptions<G3CustomerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<G3Customer> Customers { get; set; }
+    }
+}
