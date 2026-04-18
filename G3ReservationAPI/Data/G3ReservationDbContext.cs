521075b36dabbbd28e811951d4c71899b11256ec
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-04-18 00:05:00 -0400

    Add G3ReservationDbContext with Reservations DbSet

---
+++ G3ReservationAPI/Data/G3ReservationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using G3ReservationAPI.Models;
+
+namespace G3ReservationAPI.Data
+{
+    public class G3ReservationDbContext : DbContext
+    {
+        public G3ReservationDbContext(DbContextOptions<G3ReservationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<G3Reservation> Reservations { get; set; }
+    }
+}
