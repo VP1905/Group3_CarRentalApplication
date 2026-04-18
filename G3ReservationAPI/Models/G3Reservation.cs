b0eaae57dc826cd58265fa69bc5b55963787d52c
DizzaHarkhani1106 <dharkhani2375@conestogac.on.ca>
2026-04-18 00:02:43 -0400

    feat: Add G3Reservation model with required fields

---
+++ G3ReservationAPI/Models/G3Reservation.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace G3ReservationAPI.Models
+{
+    public class G3Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int VehicleId { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public string Status { get; set; } = "Active";
+    }
+}
