8d61c9a7d36085d861034cef7cd1f2dc78c1c8be
Rahel <onedayr299@gmail.com>
2026-04-19 10:22:46 -0400

    Add inventory unit tests and initial integration tests

---
+++ G3VehicleInventory.IntegrationTests/InventoryApiTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace G3VehicleInventory.IntegrationTests
+{
+    public class InventoryApiTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public InventoryApiTests(WebApplicationFactory<Program> factory)
+        {
+            _client = factory.CreateClient();
+
+            // Add this only if your WebAPI requires the gateway header
+            _client.DefaultRequestHeaders.Add("X-Internal-Gateway", "MY_GATEWAY_SECRET_KEY");
+        }
+
+        [Fact]
+        public async Task GetAllVehicles_ReturnsSuccessStatusCode()
+        {
+            var response = await _client.GetAsync("/api/Vehicles");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetVehicleById_WhenVehicleDoesNotExist_ReturnsNotFound()
+        {
+            var response = await _client.GetAsync("/api/Vehicles/999999");
+
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
