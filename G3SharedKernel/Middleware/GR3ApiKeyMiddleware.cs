42315cf2f5d8a6a983f04e4f87f8c9d8e609de8f
Nirali999 <vasantisathvara0@gmail.com>
2026-04-19 20:52:19 -0400

    Add GR3 API key middleware in shared kernel

---
+++ G3SharedKernel/Middleware/GR3ApiKeyMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+
+namespace G3SharedKernel.Middleware
+{
+    public class GR3ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string ApiKeyHeaderName = "X-API-Key";
+
+        public GR3ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("API Key was not provided.");
+                return;
+            }
+
+            var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+            var validApiKey = configuration["ApiSettings:ApiKey"];
+
+            if (string.IsNullOrWhiteSpace(validApiKey) || !validApiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
