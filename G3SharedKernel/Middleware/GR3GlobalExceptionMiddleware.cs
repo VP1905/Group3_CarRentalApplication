134a7eb8ec74b378ebd8f97b98da5d3a52296cb7
Nirali999 <vasantisathvara0@gmail.com>
2026-04-19 20:53:30 -0400

    Add GR3 global exception middleware in shared kernel

---
+++ G3SharedKernel/Middleware/GR3GlobalExceptionMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace G3SharedKernel.Middleware
+{
+    public class GR3GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GR3GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var statusCode = HttpStatusCode.InternalServerError;
+            var message = "An unexpected error occurred.";
+
+            if (exception is ArgumentException)
+            {
+                statusCode = HttpStatusCode.BadRequest;
+                message = exception.Message;
+            }
+            else if (exception is KeyNotFoundException)
+            {
+                statusCode = HttpStatusCode.NotFound;
+                message = exception.Message;
+            }
+            else if (exception is UnauthorizedAccessException)
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+                message = exception.Message;
+            }
+
+            var result = JsonSerializer.Serialize(new
+            {
+                status = (int)statusCode,
+                error = statusCode.ToString(),
+                message
+            });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
