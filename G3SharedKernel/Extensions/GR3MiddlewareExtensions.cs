2b28c2aec68e8af0dd581cdfa056de6e809dcb83
Nirali999 <vasantisathvara0@gmail.com>
2026-04-19 20:54:21 -0400

    Add GR3 middleware extension methods in shared kernel

---
+++ G3SharedKernel/Extensions/GR3MiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using G3SharedKernel.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace G3SharedKernel.Extensions
+{
+    public static class GR3MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseGR3ApiKeyMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<GR3ApiKeyMiddleware>();
+        }
+
+        public static IApplicationBuilder UseGR3GlobalExceptionMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<GR3GlobalExceptionMiddleware>();
+        }
+    }
+}
